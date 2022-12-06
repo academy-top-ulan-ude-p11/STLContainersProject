7ec54abb9c4810be3ef458594d9105b30d115757
efimov-top-academy <efimov_m@t.top-academy.ru>
2022-12-06 13:13:07 +0300

    06-12-22

--- STLContainersProject/STLContainersProject.cpp
+++ STLContainersProject/STLContainersProject.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <deque>
 #include <forward_list>
+#include <list>
 
 using namespace std;
 
@@ -21,6 +22,18 @@ class User
 {
     string name;
     int age;
+public:
+    User()
+    {
+        name = "";
+        age = 0;
+    }
+    User(string name, int age) : name{ name }, age{ age } {}
+    friend ostream& operator<<(ostream& out, const User& user)
+    {
+        out << "name: " << user.name << ", age: " << user.age;
+        return out;
+    }
 };
 
 int sum(int a, int b)
@@ -44,6 +57,14 @@ void printContainer(Iterator itBegin, Iterator ItEnd)
 {
     for (auto it = itBegin; it != ItEnd; it++)
         cout << *it << " ";
+    cout << "\n";
+}
+
+template <typename Iterator>
+void printContainerEol(Iterator itBegin, Iterator ItEnd)
+{
+    for (auto it = itBegin; it != ItEnd; it++)
+        cout << *it << "\n";
     cout << "\n";
 }
 
@@ -164,6 +185,7 @@ int main()
     printContainer(deq.begin(), deq.end());
     */
 
+    /*
     forward_list<int> list1{ 1, 6, 1, 3, 4, 5, 2, 3, 3, 6 };
     forward_list<int> list2{ 7, 7, 8, 3, 1, 9, 1, 8, 9 };
     printContainer(list1.begin(), list1.end());
@@ -179,17 +201,99 @@ int main()
     //list1.merge(list2);
     //printContainer(list1.begin(), list1.end());
 
-    /*itList2 = list2.begin();
+    itList2 = list2.begin();
     for (int i = 0; i < 3; i++) itList2++;
     for (int i = 0; i < 1; i++) itList1b++;
     for (int i = 0; i < 6; i++) itList1e++;
     list2.splice_after(itList2, list1, itList1b, itList1e);
     printContainer(list1.begin(), list1.end());
-    printContainer(list2.begin(), list2.end());*/
+    printContainer(list2.begin(), list2.end());
 
     //list1.remove(3);
     printContainer(list1.begin(), list1.end());
     //list1.remove_if(predicat1);
     list1.remove_if([](int item) { return item <= 3; });
     printContainer(list1.begin(), list1.end());
+    */
+    
+    /*
+    list<int> list1;
+
+    for (int i = 0; i < 10; i++)
+        list1.push_back(rand() % 100);
+
+    for (auto it = list1.begin(); it != list1.end(); it++)
+        cout << *it << " ";
+    cout << "\n";
+
+    cout << "front = " << list1.front() << "\n";
+    cout << "back = " << list1.back() << "\n";
+    cout << "begin = " << *list1.begin() << "\n";
+    cout << "end = " << *(--list1.end()) << "\n";
+    cout << "rbegin = " << *list1.rbegin() << "\n";
+    cout << "rend = " << *(--list1.rend()) << "\n";
+
+    auto it = list1.begin();
+    for (int i = 0; i < 3; i++)
+        ++it;
+    list1.insert(it, 100);
+    printContainer(list1.begin(), list1.end());
+
+    cout << "\n";
+    */
+
+    list<User> users;
+    users.push_back(User("Joe", 23));
+    users.push_back(User("Bob", 35));
+    users.push_back(User("Tim", 19));
+    users.push_back(User("Jim", 29));
+    users.push_back(User("Bill", 32));
+    printContainerEol(users.begin(), users.end());
+    User sam = User("Sam", 29);
+    users.insert(++++users.begin(), sam);
+    printContainerEol(users.begin(), users.end());
+    users.emplace(++++users.begin(), "Leo", 41);
+    printContainerEol(users.begin(), users.end());
+
+
+    /*auto it1 = users.begin();
+    for (int i = 0; i < 3; i++) ++it1;
+    cout << *it1 << "\n";*/
+
+    
+    /*auto it2 = users.begin();
+    for (int i = 0; i < 5; i++) ++it2;
+    cout << *it2 << "\n";
+
+    cout << "\n";
+    users.erase(it1, it2);
+    printContainerEol(users.begin(), users.end());*/
+    
+
+    /*list<int> l1{ 5, 3, 3, 3, 2, 4, 5, 5, 1, 3, 2, 3, 9, 5 };
+    list<int> l2{ 7, 3, 6, 8, 11 };*/
+
+    /*
+    auto it1 = ++++l1.begin();
+
+    auto it2_begin = ++l2.begin();
+    auto it2_end = ++++++l2.begin();
+    //l1.splice(it1, l2);
+    //l1.splice(it1, l2, it2_begin);
+    l1.splice(it1, l2, it2_begin, it2_end);
+
+    printContainer(l1.begin(), l1.end());
+    printContainer(l2.begin(), l2.end());
+    */
+
+    //printContainer(l1.begin(), l1.end());
+    ////l1.remove(5);
+    ////erase(l1, 5);
+    ////l1.remove_if([](auto item) { return item >= 5; });
+    ////l1.reverse();
+    ////erase_if(l1, [](auto item) { return item < 5; });
+    //l1.sort();
+    //l1.unique();
+    //printContainer(l1.begin(), l1.end());
+
 }
